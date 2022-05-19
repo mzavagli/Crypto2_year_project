39ddc60ad3bf8740caa203c0a27afd10d01e782a
morti <qstevens@hotmail.be>
2022-05-19 18:15:36 +0200

    [UPD] vartruncate

--- GenerateSubTables.py
+++ GenerateSubTables.py
@@ -1,3 +1,4 @@
+from cmath import tau
 from os import urandom
 from petlib.ec import EcGroup
 from math import ceil
@@ -7,6 +8,7 @@ import binascii
 
 THREAD_NUMBER = 4
 TAU = 10  # 10 bytes -> 80 bits
+INDEX_SIZE = 4
 
 # EC setup
 CURVENUMBER = 714
@@ -16,6 +18,7 @@ g = group.generator()
 o = group.order()
 n = 32  # bits wanted ( here assuming values dont exceed 32bits )
 alpha = n // 2
+beta = n - alpha
 babystep_nb = 2**alpha
 
 
@@ -43,28 +46,42 @@ def VarTruncate(tau_start, tau_stop, file):
         curr_g += g
     
     for tau in range(tau_stop, tau_start, -1):
-        A_tau = []
-        for i, elem in enumerate(file):
-            if i > (2**alpha): # not sure
-                A_tau.append(elem[tau])
-        for index, elem in enumerate(B):
-            if C[index] == 0:
-                if elem[tau] in A_tau:
-                    C[index] = tau
+        for index_file in range(2**alpha, 2**n, 2**alpha):
+            A_tau = []
+            for i in range(index_file, 2**alpha+index_file):
+                A_tau.append(file[((INDEX_SIZE+TAU)*(i))-tau: (INDEX_SIZE+TAU)*(i)])
+            for index, elem in enumerate(B):
+                if C[index] == 0:
+                    if elem in A_tau:
+                        C[index] = tau
 
     for tau in range(tau_stop, tau_start, -1):
         B_tau = {}
         for elem in B:
-            if elem[tau] not in B_tau:
-                B_tau[elem[tau]] = False
+            if elem not in B_tau:
+                B_tau[elem] = False
             else:
-                B_tau[elem[tau]] = True
+                B_tau[elem] = True
 
         for index, elem in enumerate(B):
-            if (B_tau[elem[tau]] == True) and (C[index] < tau):
+            if (B_tau[elem] == True) and (C[index] < tau):
                 C[index] = tau
     
     return C
+
+
+def CreateLookupTable(tau_start, tau_stop, file):
+    C = VarTruncate(tau_start, tau_stop, file)
+    file_dic = {}
+    for i in range(tau_start, tau_stop):
+        file_dic[i] = open(f'tau_{i}', 'ab')
+
+    with open(file, "rb") as f:
+        for index, elem in enumerate(C):
+            file_dic[elem].write(file[((INDEX_SIZE+TAU)*(index))-elem: (INDEX_SIZE+TAU)*(index)])
+    
+    for file in file_dic.values():
+        file.close()
 
 def main():
     divided_m = ceil(babystep_nb/THREAD_NUMBER)
